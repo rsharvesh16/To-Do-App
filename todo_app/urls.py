7ae96d757e5660d6e5fbc77c2e60b2d6af7acd69
rsharvesh16 <rsharvesh16@gmail.com>
2024-02-12 23:41:15 +0530

    Updated URLs for Deletion

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("list/<int:list_id>/",
         views.ItemListView.as_view(), name="list"),
     path("list/add/", views.ListCreate.as_view(), name = "list-add"),
+    path("list/<int:pk>/delete/", views.ListDelete.as_view(), name = "list-delete"),
     path(
         "list/<int:list_id>/item/add/",
         views.ItemCreate.as_view(),
@@ -15,4 +16,9 @@ urlpatterns = [
         views.ItemUpdate.as_view(),
         name="item-update",
     ), 
+    path(
+        "list/<int:list_id>/item/<int:pk>/delete/",
+        views.ItemDelete.as_view(),
+        name="item-delete",
+    ),
 ]
